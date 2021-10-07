f5e1bfd1eefa6f434b957ec186608da7c4836da6
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-07 13:39:09 +0200

    Codice completo

---
+++ javascript/prompt.js
@@ -0,0 +1,4 @@
+const userName = prompt("What's your name?");
+const userSurname = prompt("What's your surname?")
+const userColor = prompt("What's your favourite color?");
+alert('The generated password is: ' + userName + userSurname + userColor + "21")
